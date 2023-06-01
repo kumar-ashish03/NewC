9983344d26f99eb313d9e863bf3329e1846265da
kumar-ashish03 <135213235+kumar-ashish03@users.noreply.github.com>
2023-06-01 14:10:56 +0530

    Create NewC.c
    New component main file

---
+++ NewC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+  printf("Hello World....\n\n New Component \n		By Ashish);
+
+  return 0;
+}
