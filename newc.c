bdf0145b5aceb2aeea7e4a9b4f5a9077214aa6e5
kumar-ashish03 <135213235+kumar-ashish03@users.noreply.github.com>
2023-06-09 11:30:51 +0530

    Update newc.c

--- newc.c
+++ newc.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-void patch1()
-{
-  printf("patch applied");
-}
+
 int main(int argc, char *argv[])
 {
-  printf("Hello World....\n\n New Component \n		By Ashish");
-  patch1();
+  printf("Hello World....\n\n New Component \n		By Ashish");  patch1();
   return 0;
 }
